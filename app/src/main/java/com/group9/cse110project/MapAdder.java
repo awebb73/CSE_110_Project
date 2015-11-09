7f018f408a684dd0ef7d754e3b94900784c7918e
art <awebb73@gmail.com>
2015-11-08 21:02:56 -0800

    Initial Commit

---
+++ app/src/main/java/com/group9/cse110project/MapAdder.java
@@ -0,0 +1,87 @@
+package com.group9.cse110project;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapAdder extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private RatingBar ratingBar;
+    //private Button fin;
+    private int stars;
+    // adding some comments as a test for git hub
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_adder);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        Button fin = (Button) findViewById(R.id.button);
+        fin.setEnabled(false);
+        addListenerOnRatingBar();
+
+    }
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // ART -- Add a marker in Sydney and move the camera
+        LatLng ucsd = new LatLng(32.881145, -117.2374);
+        mMap.addMarker(new MarkerOptions().position(ucsd).title("UCSD"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ucsd, 17));
+    }
+    // ART --gathers the stars num data and can be pushed to the first intent
+    public void fin(View view) {
+        stars = (int)ratingBar.getRating();
+        Intent intent = new Intent(MapAdder.this, MapsActivity.class);
+        startActivity(intent);
+    }
+    // ART -- listens to the rating bar and actives he confirnation screen
+    public void addListenerOnRatingBar() {
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+
+        // ART -- if the value is changed
+        // ART -- we allow the confirmation button to become activated
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                stars = (int) ratingBar.getRating();
+                // ART -- command to see logcat output
+                // Log.d("Checking Stars value: ", String.valueOf(stars));
+                // ART -- if else block to only turn on the confirmation button as required
+                if (stars == 0) {
+                    Button fin = (Button) findViewById(R.id.button);
+                    fin.setEnabled(false);
+                }
+                else if (stars > 0) {
+                    Button fin = (Button) findViewById(R.id.button);
+                    fin.setEnabled(true);
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/group9/cse110project/MapsActivity.java
+++ app/src/main/java/com/group9/cse110project/MapsActivity.java
@@ -1,17 +1,29 @@
 package com.group9.cse110project;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.EditText;
+//handle adding a pin on pin drop
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleMap.OnMapClickListener,   GoogleMap.OnMarkerClickListener {
+    public final static String EXTRA_MESSAGE = "com.group9.cse110project.MESSAGE";
     private GoogleMap mMap;
 
     @Override
@@ -38,9 +50,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setMyLocationEnabled(true);
+        LocationManager locate = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        System.out.println(locate);
+        Criteria crit = new Criteria();
+        String provider = locate.getBestProvider(crit, true);
+        mMap.setOnMapClickListener(this);
+        LatLng geisel = new LatLng(32.881145, -117.237394);
+        //mMap.addMarker(new MarkerOptions().position(geisel).title("Geisel Library"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(geisel, 17));
     }
+    @Override
+    public boolean onMarkerClick(Marker arg0) {
+        arg0.showInfoWindow();
+        return true;
+    }
+    @Override
+    //handles adding in markers on the map
+    public void onMapClick(LatLng latLng) {
+        float zoomLevel = 16;
+        this.mMap.addMarker(new MarkerOptions().position(latLng).title("restroom"));
+        this.mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, MapAdder.class);
+        //EditText tex = (EditText) findViewById(R.id.text);
+        //String message = tex.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
