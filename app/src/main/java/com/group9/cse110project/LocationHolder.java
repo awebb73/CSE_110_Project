75edd6c373639b4373dceae847284d188e677956
jade <supershake8@gmail.com>
2015-11-16 15:44:02 -0800

    LocationHolder implements parcelable

--- app/src/main/java/com/group9/cse110project/LocationHolder.java
+++ app/src/main/java/com/group9/cse110project/LocationHolder.java
@@ -1,16 +1,21 @@
 package com.group9.cse110project;
 
+import android.location.LocationManager;
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.android.gms.maps.model.LatLng;
+
 
 /**
  * Created by art on 11/16/15.
  */
-public class LocationHolder {
-
-
+public class LocationHolder implements Parcelable {
     private String sent;
     private int rating;
     public LatLng loc;
+
+
 
     // 3 arg constructor
     // int to hold the star rating
@@ -18,9 +23,13 @@ public class LocationHolder {
     // string
     public LocationHolder(String e, int rating, LatLng loc)
     {
-        this.sent = sent;
+        this.sent = e;
         this.rating = rating;
         this.loc = loc;
+    }
+
+    private LocationHolder(Parcel in){
+        readFromParcel(in);
     }
 
     // getter for the string
@@ -47,4 +56,33 @@ public class LocationHolder {
     public void setLoc(LatLng loc) {
         this.loc = loc;
     }
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(sent);
+        dest.writeInt(rating);
+        dest.writeParcelable(loc, flags);
+    }
+
+    private void readFromParcel(Parcel source){
+        this.sent = source.readString();
+        this.rating = source.readInt();
+        this.loc = source.readParcelable(LatLng.class.getClassLoader());
+
+    }
+
+    public static final Parcelable.Creator<LocationHolder> CREATOR = new Parcelable.Creator<LocationHolder>(){
+        @Override
+        public LocationHolder createFromParcel(Parcel in){
+            return new LocationHolder(in);
+        }
+        @Override
+        public LocationHolder[] newArray(int size){
+            return new LocationHolder[size];
+        }
+    };
 }
